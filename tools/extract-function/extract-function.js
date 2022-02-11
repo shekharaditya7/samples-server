f39edef62c4476c4d6850432fefbe370049cce5a
adityaShekhar <shekharaditya7@gmail.com>
2022-02-11 17:54:26 +0530

    trial commit

--- tools/extract-function/extract-function.js
+++ tools/extract-function/extract-function.js
@@ -2,10 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Include required node modules
-
 var express = require("express");
 var fnExtractor = require("function-extractor");
 
 var app = express();
-
